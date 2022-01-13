c3d125e65f64d7e0b7d080f850964d059858b18c
ovoloshk <ovoloshk@softserveinc.com>
2022-01-13 21:45:34 +0200

    Last commit

--- src/main/java/com/softserveinc/Task01.java
+++ src/main/java/com/softserveinc/Task01.java
@@ -20,23 +20,19 @@ public class Task01 {
         double[] temperature = { -1.7, -1.0, 2.6,  9.0, 15.1, 19.4, 21.4, 21.2, 17.1, 11.1, 5.9,  1.4 };
         String[] month = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII"};
         double averageTemperature = temperature[0];
-        double minTemperature = temperature[0];
-        double maxTemperature = temperature[0];
-        String monthMinTemperature = month[0];
-        String monthMaxTemperature = month[0];
+        int min = 0;
+        int max = 0;
         for (int i = 1; i < temperature.length; i++){
             averageTemperature += temperature[i];
-            if (temperature[i]<minTemperature) {
-                minTemperature = temperature[i];
-                monthMinTemperature = month[i];
-            } else if (temperature[i]>maxTemperature) {
-                maxTemperature = temperature[i];
-                monthMaxTemperature = month[i];
+            if (temperature[i]<temperature[min]) {
+                min = i;
+            } else if (temperature[i]>temperature[max]) {
+                max = i;
             }
         }
         averageTemperature /= temperature.length;
         System.out.println("Average annual temperature: " + averageTemperature);
-        System.out.println("Month with the lowest temperature in a year: " + monthMinTemperature);
-        System.out.println("The month with the highest temperatures in a year: " + monthMaxTemperature);
+        System.out.println("Month with the lowest temperature in a year: " + month[min]);
+        System.out.println("The month with the highest temperatures in a year: " + month[max]);
     }
 }
